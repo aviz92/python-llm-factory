041bf22f55e4f99da1993347e456bcce94245bd4
avizaguri-macbook <avi210892@gmail.com>
2025-07-11 20:31:39 +0300

    Add initial project structure with LLMFactory implementation and configuration

---
+++ main.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel, Field
+
+from llm_factory.llm_factory import LLMProvider, LLMFactory
+
+if __name__ == "__main__":
+    class CompletionModel(BaseModel):
+        response: str = Field(description="Your response to the user.")
+        reasoning: str = Field(description="Explain your reasoning for the response.")
+
+
+    messages = [
+        {
+            "role": "system",
+            "content": "You are a helpful assistant."},
+        {
+            "role": "user",
+            "content": "If it takes 2 hours to dry 1 shirt out in the sun, how long will it take to dry 5 shirts?",
+        },
+    ]
+
+    # llm = LLMFactory(provider=LLMProvider.GEMINI)
+    llm = LLMFactory(provider="gemini")
+    completion = llm.create_completion(
+        response_model=CompletionModel,
+        messages=messages,
+    )
+    assert isinstance(completion, CompletionModel)
+
+    print(f"Response: {completion.response}\n")
+    print(f"Reasoning: {completion.reasoning}")
