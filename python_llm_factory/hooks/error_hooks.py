003fa0db3776e61ff26ac47b9f0a3bcb05ed1581
avizaguri-macbook <avi210892@gmail.com>
2025-10-17 19:31:39 +0300

    hooks

--- python_llm_factory/hooks/error_hooks.py
+++ python_llm_factory/hooks/error_hooks.py
@@ -1,21 +1,22 @@
 from typing import Callable
 
+from custom_python_logger import get_logger
 from instructor.core import HookName
 
 from python_llm_factory import LLMFactory
 
+logger = get_logger(__name__)
+
 
 def log_exception(exception: Exception):
-    print(f"An exception occurred: {str(exception)}")
+    logger.debug(f"An exception occurred: {str(exception)}")
 
 
 def add_error_hooks(client: LLMFactory, handler: Callable):
     client.client.on(HookName.COMPLETION_ERROR, handler)
+    logger.info(f"Completion hook registered for {client.client.name}")
 
 
 def stop_error_hooks(client: LLMFactory, handler: Callable):
     client.client.off(HookName.COMPLETION_ERROR, handler)
-
-
-def clear_error_hooks(client: LLMFactory):
-    client.client.remove_all_listeners(HookName.COMPLETION_ERROR)
+    logger.info(f"Completion hook stopped for {client.client.name}")
--- python_llm_factory/hooks/logging_hooks.py
+++ python_llm_factory/hooks/logging_hooks.py
@@ -1,22 +1,25 @@
 from typing import Callable
 
-from custom_python_logger import json_pretty_format
+from custom_python_logger import get_logger, json_pretty_format
 from instructor.core import HookName
 
 from python_llm_factory import LLMFactory
 
+logger = get_logger(__name__)
 
 def log_kwargs(**kwargs) -> None:
-    print(f"Function called with kwargs: {json_pretty_format(kwargs)}")
+    logger.debug(f"Function called with kwargs: {json_pretty_format(kwargs)}")
 
 
 def log_completion_response(response) -> None:
-    print(f"Completion response: {json_pretty_format(response)}")
+    logger.debug(f"Completion response: {json_pretty_format(response)}")
 
 
 def add_logging_hooks(client: LLMFactory, handler: Callable) -> None:
     client.client.on(HookName.COMPLETION_KWARGS, handler)
+    logger.info(f"Completion logging hook registered for {client.client.name}")
 
 
 def stop_logging_hooks(client, handler):
     client.client.off(HookName.COMPLETION_KWARGS, handler)
+    logger.info(f"Completion logging hook stopped for {client.client.name}")
