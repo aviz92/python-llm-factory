defa0456bf69b28a32c089b0575fcdbef2b37113
avizaguri-macbook <avi210892@gmail.com>
2025-10-17 19:16:02 +0300

    examples

--- examples/tools/create_jira_bug.py
+++ examples/tools/create_jira_bug.py
@@ -1,2 +1,20 @@
 def create_jira_bug(title: str) -> dict:
     return {"bug_id": "JIRA-1234", "title": title}
+
+
+function_descriptions = {  # pylint: disable=R6101
+    "create_jira_bug": {
+        "name": "create_jira_bug",
+        "description": "Creates a new Jira bug with the given title.",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "title": {
+                    "type": "string",
+                    "description": "The title of the Jira bug to be created.",
+                },
+            },
+            "required": ["title"],
+        },
+    },
+}
--- examples/tools/notify_slack.py
+++ examples/tools/notify_slack.py
@@ -1,3 +1,21 @@
 def notify_slack(message: str) -> dict:
     print("Sending Slack message:", message)
     return {"status": "sent", "message": message}
+
+
+function_descriptions = {  # pylint: disable=R6101
+    "notify_slack": {
+        "name": "notify_slack",
+        "description": "Sends a Slack message with the given content.",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "message": {
+                    "type": "string",
+                    "description": "The content of the Slack message to be sent.",
+                },
+            },
+            "required": ["message"],
+        },
+    },
+}
